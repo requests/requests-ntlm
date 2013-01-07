9d14c8212802a745c0e845298ffa9d64642bddf3
Ben Toews <mastahyeti@gmail.com>
2013-01-07 14:10:08 -0600

    manually merging fix_license

--- setup.py
+++ setup.py
@@ -3,30 +3,17 @@
 
 from setuptools import setup
 
-license = '''
-Copyright (c) 2012 Ben Toews
-
-Permission to use, copy, modify and/or distribute this software for any
-purpose with or without fee is hereby granted, provided that the above
-copyright notice and this permission notice appear in all copies.
-
-THE SOFTWARE IS PROVIDED "AS-IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-'''.strip()
+with open('LICENSE','r') as f:
+  license = f.read().strip()
 
 setup(
-    name                 = 'requests_ntlm',
-    version              = '0.0.2',
-    packages             = [ 'requests_ntlm' ],
-    requires             = [ 'requests(>=1.0.0)', 'ntlm' ],
-    provides             = [ 'requests_ntlm' ],
-    author               = 'Ben Toews',
-    author_email         = 'mastahyeti@gmail.com',
-    description          = 'This package allows for HTTP NTLM authentication using the requests library.',
-    license              = license
+    name         = 'requests_ntlm',
+    version      = '0.0.2.1',
+    packages     = [ 'requests_ntlm' ],
+    requires     = [ 'requests(>=1.0.0)', 'ntlm' ],
+    provides     = [ 'requests_ntlm' ],
+    author       = 'Ben Toews',
+    author_email = 'mastahyeti@gmail.com',
+    description  = 'This package allows for HTTP NTLM authentication using the requests library.',
+    license      = license
 )
