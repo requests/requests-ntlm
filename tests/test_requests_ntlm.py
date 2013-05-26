fade2d82ccc1d563504f979a072b93f3f768b176
Cory Benfield <lukasaoz@gmail.com>
2013-05-26 09:28:44 +0100

    Tests are good.

--- tests/test_requests_ntlm.py
+++ tests/test_requests_ntlm.py
@@ -19,5 +19,20 @@ class TestRequestsNtlm(unittest.TestCase):
 
         self.assertEqual(res.status_code,200)
 
+    def test_history_is_preserved(self):
+        res = requests.get(url=self.test_server_url,
+                           auth=requests_ntlm.HttpNtlmAuth(self.test_server_username,
+                                                           self.test_server_password))
+
+        self.assertEqual(len(res.history), 2)
+
+    def test_new_requests_are_used(self):
+        res = requests.get(url=self.test_server_url,
+                           auth=requests_ntlm.HttpNtlmAuth(self.test_server_username,
+                                                           self.test_server_password))
+
+        self.assertTrue(res.history[0].request is not res.history[1].request)
+        self.assertTrue(res.history[0].request is not res.request)
+
 if __name__ == '__main__':
     unittest.main()
