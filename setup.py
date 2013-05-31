26db7c1f6af873db4684b69fda1793ecdcc58cf9
David Fischer <djfische@gmail.com>
2013-05-31 07:39:45 -0700

    Other setup.py improvements
    * ISC license
    * Add classifiers

--- setup.py
+++ setup.py
@@ -3,17 +3,22 @@
 
 from setuptools import setup
 
-with open('LICENSE','r') as f:
-  license = f.read().strip()
-
 setup(
-    name         = 'requests_ntlm',
-    version      = '0.0.2.1',
-    packages     = [ 'requests_ntlm' ],
-    requires     = [ 'requests(>=1.0.0)', 'ntlm' ],
-    provides     = [ 'requests_ntlm' ],
-    author       = 'Ben Toews',
-    author_email = 'mastahyeti@gmail.com',
-    description  = 'This package allows for HTTP NTLM authentication using the requests library.',
-    license      = license
+    name='requests_ntlm',
+    version='0.0.2.1',
+    packages=[ 'requests_ntlm' ],
+    requires=[ 'requests(>=1.0.0)', 'ntlm' ],
+    provides=[ 'requests_ntlm' ],
+    author='Ben Toews',
+    author_email='mastahyeti@gmail.com',
+    description='This package allows for HTTP NTLM authentication using the requests library.',
+    license='ISC',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: ISC License (ISCL)',
+    ],
 )
