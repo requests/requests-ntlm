638350329f243dfc0864c9ae2064f5089d953188
Erik Cederstrand <erik@cederstrand.dk>
2014-03-18 10:00:02 +0100

    Strip spaces around multiple cookie values
    http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html section 14.47 only mentions comma-separation, not comma+space separation. So split only by comma, but clean up any spaces afterwards.

--- requests_ntlm/requests_ntlm.py
+++ requests_ntlm/requests_ntlm.py
@@ -55,7 +55,7 @@ class HttpNtlmAuth(AuthBase):
 
         # get the challenge
         auth_header_value = response2.headers[auth_header_field]
-        ntlm_header_value = filter(lambda s: s.startswith('NTLM '), auth_header_value.split(','))[0]
+        ntlm_header_value = filter(lambda s: s.startswith('NTLM '), auth_header_value.split(','))[0].strip()
         ServerChallenge, NegotiateFlags = ntlm.parse_NTLM_CHALLENGE_MESSAGE(ntlm_header_value[5:])
 
         # build response
