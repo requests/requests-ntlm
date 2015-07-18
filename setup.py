35252865821fdada0f39090880b280145eed6a79
Cory Benfield <lukasaoz@gmail.com>
2015-07-18 10:01:48 +0100

    Bump requests dependency to 2.0.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='requests_ntlm',
     version='0.1.0',
     packages=[ 'requests_ntlm' ],
-    install_requires=[ 'requests>=1.0.0', 'python-ntlm3' ],
+    install_requires=[ 'requests>=2.0.0', 'python-ntlm3' ],
     provides=[ 'requests_ntlm' ],
     author='Ben Toews',
     author_email='mastahyeti@gmail.com',
