fea4f5d5cb04145fdc17959b26450336ce8027c4
jborean93 <jborean93@gmail.com>
2016-08-29 16:08:03 +1000

    Add ntlm2 support (#1)
    * Changed dependency of python-ntlm3 to ntlm-auth to support NTLMv2 authentication and the various features brought on by that. Fixed up test cases now we are using a newer version of NTLM. Added appveyor and travis-ci functionality for better testing
    
    * Bumped ntlm-auth dependency for python 2.6 changes. Added background jobs to start the flask instance for both travis and appveyor
    
    * trying to get appveyor working now
    
    * more tests for coverage, appveyor still not working
    
    * Adding __name__ to functional to get appveyor working. Now that we have more coverage have added more tests
    
    * appveyor is working now, trying to get it to wait until flask is ready
    
    * that didn't work, moving flask startup to before we configure IIS to give it time
    
    * trying to get the tests working again
    
    * trying different tactic for appveyor now
    
    * hopefully the last, it worked locally
    
    * removed Stop-Job in appveyor as the process will be killed at the end anyway
    
    * Added wheel dependency for appveyor
    
    * trying to fix up import errors for newer version of python
    
    * let's see if this works
    
    * I really don't like imports
    
    * verify weird import issues, hopefully this will work
    
    * trying to get powershell arguments right
    
    * looks like quotes don't work normally, trying single quotes in quotes

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,15 +1,10 @@
+import mock
+
 from flask import Flask,request
-from ntlm3 import ntlm
+from ntlm_auth import ntlm
+from tests.utils import *
+
 app = Flask(__name__)
-
-REQUEST_2_TEMPLATE = '{0} %s' % ntlm.create_NTLM_NEGOTIATE_MESSAGE("domain\\username").decode('ascii')
-
-RESPONSE_2_NONCE = 'TlRMTVNTUAACAAAADAAMADAAAAAHAgMAESIzRFVmd4gAAAAAAAAAAGIAYgA8AAAARABPAE0AQQBJAE4AAgAMAEQATwBNAEEASQBOAAEADABTAEUAUgBWAEUAUgAEABYAZQB4AGEAbQBwAGwAZQAuAGMAbwBtAAMAJABTAEUAUgBWAEUAUgAuAGUAeABhAG0AcABsAGUALgBjAG8AbQAAAAAA'
-RESPONSE_2_TEMPLATE = '{0} %s' % RESPONSE_2_NONCE
-
-ServerChallenge, NegotiateFlags = ntlm.parse_NTLM_CHALLENGE_MESSAGE(RESPONSE_2_NONCE)
-
-REQUEST_3_TEMPLATE = '{0} %s' % ntlm.create_NTLM_AUTHENTICATE_MESSAGE(ServerChallenge, 'username', 'domain', 'password', NegotiateFlags).decode('ascii')
 
 @app.route("/ntlm")
 def ntlm_auth():
@@ -23,23 +18,48 @@ def negotiate_auth():
 def negotiate_and_ntlm_auth():
     return get_auth_response('NTLM', advertise_nego_and_ntlm=True)
 
-def get_auth_response(auth_type, advertise_nego_and_ntlm=False):
-    response_headers = {'WWW-Authenticate':auth_type if not advertise_nego_and_ntlm else 'Negotiate, NTLM'}
-    status_code = 401
-    response = "auth with 'domain\\username':'password'"
+# With NTLMv2 we need to mock out some functions that usually return random values to ensure we get the same message
+@mock.patch('os.urandom', side_effect=mock_random)
+@mock.patch('ntlm_auth.messages.get_version', side_effect=mock_version)
+@mock.patch("ntlm_auth.messages.get_random_export_session_key", side_effect=mock_random_session_key)
+@mock.patch('ntlm_auth.compute_response.get_windows_timestamp', side_effect=mock_timestamp)
+def get_auth_response(auth_type, mock_random, mock_version, mock_session_key, mock_timestamp, advertise_nego_and_ntlm=False):
+    # Create the NTLM context on the server for validation with requests_ntlm
+    ntlm_context = ntlm.Ntlm()
+    negotiate_response = ntlm_context.create_negotiate_message(domain.upper()).decode('ascii')
 
-    # 2nd request
-    if request.headers.get('Authorization','') == REQUEST_2_TEMPLATE.format(auth_type):
-        response_headers = {'WWW-Authenticate':RESPONSE_2_TEMPLATE.format(auth_type)}
+    challenge_response = 'TlRMTVNTUAACAAAAAwAMADgAAAAzgoriASNFZ4mrze8AAAAAAAAAACQAJABEAAAABgBwFwAAAA9TAGUA' \
+                         'cgB2AGUAcgACAAwARABvAG0AYQBpAG4AAQAMAFMAZQByAHYAZQByAAAAAAA='
+    ntlm_context.parse_challenge_message(challenge_response)
+
+    auth_response = ntlm_context.create_authenticate_message(username, password, domain.upper()).decode('ascii')
+
+    # Set the default responses
+    response_headers = {'WWW-Authenticate': auth_type if not advertise_nego_and_ntlm else 'Negotiate, NTLM'}
+    status_code = 401
+    response = "auth with '%s\\%s':'%s'" % (domain, username, password)
+
+    # Create the expectation headers to use below
+    negotiate_header = auth_type + ' ' + negotiate_response
+    challenge_header = auth_type + ' ' + challenge_response
+    authenticate_header = auth_type + ' ' + auth_response
+
+    # Get the actual header that is returned by requests_ntlm
+    actual_header = request.headers.get('Authorization', '')
+
+    # Server received the negotiate token and validates it matches what we expect
+    if actual_header == negotiate_header:
+        # Add the challenge token and return it with the response headers
+        response_headers = {'WWW-Authenticate': challenge_header}
         status_code = 401
 
-    # 3rd request
-    elif request.headers.get('Authorization','') == REQUEST_3_TEMPLATE.format(auth_type):
+    # Server received the authenticate token and validates it matches what we expect
+    elif actual_header == authenticate_header:
         response_headers = {}
         status_code = 200
-        response = "authed"
+        response = 'authed'
 
-    return response,status_code,response_headers
+    return response, status_code, response_headers
 
 if __name__ == "__main__":
     app.run()
---
+++ tests/unit/test_requests_ntlm.py
@@ -0,0 +1,97 @@
+import unittest
+import requests
+import requests_ntlm
+import mock
+
+from tests.utils import *
+
+"""
+In these tests we need to mock out the 4 different methods to ensure timestamp and random nonces return the same value
+in all our tests.
+"""
+
+class TestRequestsNtlm(unittest.TestCase):
+
+    def setUp(self):
+        self.test_server_url        = 'http://localhost:5000/'
+        self.test_server_username   = '%s\\%s' % (domain, username)
+        self.test_server_password   = password
+        self.auth_types = ['ntlm','negotiate','both']
+
+    @mock.patch('os.urandom', side_effect=mock_random)
+    @mock.patch('ntlm_auth.messages.get_version', side_effect=mock_version)
+    @mock.patch("ntlm_auth.messages.get_random_export_session_key", side_effect=mock_random_session_key)
+    @mock.patch('ntlm_auth.compute_response.get_windows_timestamp', side_effect=mock_timestamp)
+    def test_requests_ntlm(self, mock_random, mock_version, mock_session_key, mock_timestamp):
+        for auth_type in self.auth_types:
+            res = requests.get(\
+                url  = self.test_server_url + auth_type,\
+                auth = requests_ntlm.HttpNtlmAuth(
+                    self.test_server_username,\
+                    self.test_server_password))
+
+            self.assertEqual(res.status_code,200, msg='auth_type ' + auth_type)
+
+    @mock.patch('os.urandom', side_effect=mock_random)
+    @mock.patch('ntlm_auth.messages.get_version', side_effect=mock_version)
+    @mock.patch("ntlm_auth.messages.get_random_export_session_key", side_effect=mock_random_session_key)
+    @mock.patch('ntlm_auth.compute_response.get_windows_timestamp', side_effect=mock_timestamp)
+    def test_history_is_preserved(self, mock_random, mock_version, mock_session_key, mock_timestamp):
+        for auth_type in self.auth_types:
+            res = requests.get(url=self.test_server_url + auth_type,
+                               auth=requests_ntlm.HttpNtlmAuth(self.test_server_username,
+                                                               self.test_server_password))
+
+            self.assertEqual(len(res.history), 2)
+
+    @mock.patch('os.urandom', side_effect=mock_random)
+    @mock.patch('ntlm_auth.messages.get_version', side_effect=mock_version)
+    @mock.patch("ntlm_auth.messages.get_random_export_session_key", side_effect=mock_random_session_key)
+    @mock.patch('ntlm_auth.compute_response.get_windows_timestamp', side_effect=mock_timestamp)
+    def test_new_requests_are_used(self, mock_random, mock_version, mock_session_key, mock_timestamp):
+        for auth_type in self.auth_types:
+            res = requests.get(url=self.test_server_url + auth_type,
+                               auth=requests_ntlm.HttpNtlmAuth(self.test_server_username,
+                                                               self.test_server_password))
+
+            self.assertTrue(res.history[0].request is not res.history[1].request)
+            self.assertTrue(res.history[0].request is not res.request)
+
+    def test_username_parse_backslash(self):
+        test_user = 'domain\\user'
+        expected_domain = 'DOMAIN'
+        expected_user = 'user'
+
+        context = requests_ntlm.HttpNtlmAuth(test_user, 'pass')
+
+        actual_domain = context.domain
+        actual_user = context.username
+
+        assert actual_domain == expected_domain
+        assert actual_user == expected_user
+
+    def test_username_parse_at(self):
+        test_user = 'user@domain'
+        expected_domain = 'DOMAIN'
+        expected_user = 'user'
+
+        context = requests_ntlm.HttpNtlmAuth(test_user, 'pass')
+
+        actual_domain = context.domain
+        actual_user = context.username
+
+        assert actual_domain == expected_domain
+        assert actual_user == expected_user
+
+    def test_username_parse_no_domain(self):
+        test_user = 'user'
+        expected_domain = '.'
+        expected_user = 'user'
+
+        context = requests_ntlm.HttpNtlmAuth(test_user, 'pass')
+
+        actual_domain = context.domain
+        actual_user = context.username
+
+        assert actual_domain == expected_domain
+        assert actual_user == expected_user
