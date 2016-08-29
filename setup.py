d40012407dad1bf9a14b75ae8f01322c5d2ce1e6
BOREAN, Jordan <Jordan.BOREAN@suncorp.com.au>
2016-08-29 14:14:41 +1000

    Bumped ntlm-auth dependency for python 2.6 changes. Added background jobs to start the flask instance for both travis and appveyor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='requests_ntlm',
     version='0.3.0',
     packages=[ 'requests_ntlm' ],
-    install_requires=[ 'requests>=2.0.0', 'ntlm-auth' ],
+    install_requires=[ 'requests>=2.0.0', 'ntlm-auth>=1.0.2' ],
     provides=[ 'requests_ntlm' ],
     author='Ben Toews',
     author_email='mastahyeti@gmail.com',
